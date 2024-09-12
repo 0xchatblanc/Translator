bb089675497fd3532a8b1a3d00e1a733b1bfbaf0
The Ghost <mael.demons17@gmail.com>
2024-09-12 09:18:08 +0200

    Commit v 0.1.1 (Gui ...)

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ theme : str = "dark"
 set_appearance_mode(theme)
 icon_translate = Image.open(f'images/translate_for_{theme}.png')
 
-btn = CTkButton(master=app, text="Translate", corner_radius=24, fg_color="transparent",
+label = CTkLabel(master=app, text="Welcome on my translator", font=("Arial", 25),text_color="#32b878")
+btn = CTkButton(master=app, text="Translate", corner_radius=12, fg_color="transparent",
                 hover_color="#40ad79", border_color="#2e855b",
-                border_width=2, image=CTkImage(dark_image=icon_translate, light_image=icon_translate))
+                border_width=2, image=CTkImage(dark_image=icon_translate, light_image=icon_translate, size=(20,20)))
 
+label.place(relx=.5, rely=.1, anchor="n")
 btn.place(relx=.5, rely=.5, anchor="center")
 app.mainloop()
